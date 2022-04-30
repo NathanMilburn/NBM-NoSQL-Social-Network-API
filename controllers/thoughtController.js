ad10c75c4d80f5c34ec104355a0f1f20ebe321fa
NathanM <nathan.milburn@outlook.com>
2022-04-30 14:12:15 -0700

    created thought controllers. Still need to finish controllers for reactions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,62 @@
 const { User, Thought } = require('../models');
+const reactionSchema = require('../models/Reaction');
+
+const thoughtController = {
+    getThoughts(req, res) {
+        Thought.find()
+        .then((thoughts) => {
+            res.status(200).json(thoughts);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+    },
+    getThoughtById(req, res) {
+        Thought.findById({ _id: req.params.thoughtId})
+        .then((thoughts) => {
+            res.status(200).json(thoughts);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
+    },
+    createNewThought(req, res) {
+        Thought.create(req.body)
+        .then((thoughts) => {
+            res.status(200).json(thoughts);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
+    },
+    updateThought(req, res) {
+        Thought.findByIdAndUpdate(
+            { _id: req.params.id },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((thoughts) => {
+                res.status(200).json(thoughts);
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    },
+    deleteThought(req, res) {
+        Thought.deleteOne({ _id: req.params.id })
+            .then((thoughts) => {
+                res.status(200).json(thoughts);
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    },
+    createReaction(req, res) {
+        Thought.create
+    },
+    deleteReaction(req, res) {
+        reaction
+    }
+};
+
+module.exports = thoughtController;
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const { Thought } = require('../../models');
+const {getThoughts, getThoughtById, createNewThought, updateThought, deleteThought, createReaction, deleteReaction} = require('../../controllers/thoughtController')
 
-router.get('/', async (req, res) => {
-    res.status(200).json(`Howdy! this is my api/thoughts route`)
-});
+// router.get('/', async (req, res) => {
+//     res.status(200).json(`Howdy! this is my api/thoughts route`)
+// });
+
+router.route('/').get(getThoughts).post(createNewThought);
+router.route('/:id').get(getThoughtById).delete(deleteThought).put(updateThought);
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction)
 
 module.exports = router;
